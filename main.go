3c9851f627e48b4e470b10ebb9bf1d817d9f80c9
Kishore Chandra <kishorechandra.developer@gmail.com>
2024-12-28 16:58:50 +0530

    Refactor main.go to consolidate route setup and add missing DELETE route for user subscriptions

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
+// main.go
 package main
 
 import (
 	"log"
 
 	"github.com/chandra-devs/subscription_app/config"
+	"github.com/chandra-devs/subscription_app/controllers"
 	"github.com/chandra-devs/subscription_app/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -19,10 +21,20 @@ func main() {
 	config.ConnectDB()
 	config.InitJWTConfig()
 
-	// Setup routes
-	api := app.Group("/api/v1")
-	routes.SetupAuthRoutes(api)
-	routes.SetupSubscriptionRoutes(api)
+	// Setup all routes using the existing SetupRoutes function
+	routes.SetupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
+}
+
+// routes/setup.go - update only the SetupUserRoutes function
+func SetupUserRoutes(router fiber.Router) {
+	users := router.Group("/users")
+	users.Get("/", controllers.GetUsers)
+	users.Get("/:id", controllers.GetUser)
+	users.Post("/", controllers.CreateUser)
+	users.Put("/:id", controllers.UpdateUser)
+	users.Delete("/:id", controllers.DeleteUser) // Add the missing DELETE route
+	users.Get("/:id/subscriptions", controllers.GetUserSubscriptions)
+	users.Post("/:id/subscriptions", controllers.AddSubscription)
 }
